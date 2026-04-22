7e0e3b11486d0b578dcb6f2d6b73f1117df3eec5
Ana Vitoria Maciel <anavitoriamacielr@gmail.com>
2026-04-22 20:36:46 -0300

    feat: atualiza visual com cores do Viriato Correa e logo da escola

--- escola/migrations/0004_merge.py
+++ escola/migrations/0004_merge.py
@@ -1,11 +1,8 @@
 from django.db import migrations
 
-
 class Migration(migrations.Migration):
-
     dependencies = [
         ('escola', '0003_alter_notamateria_options'),
-        ('escola', '0004_perfilturma_registroassiduidade_presencaaluno'),
+        ('escola', '0003_questao_dificuldade'),
     ]
-
     operations = []
--- gestao_escolar/settings.py
+++ gestao_escolar/settings.py
@@ -22,6 +22,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://frontend:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
